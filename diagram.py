4ab804472287a44a2c154a8eeb50fd955ebaf7c3
juanchovelezpro <velezolaya2012@hotmail.com>
2020-09-25 13:15:26 -0500

    Diagram update

--- diagram.py
+++ diagram.py
@@ -9,36 +9,28 @@ from diagrams.aws.storage import S3
 
 with Diagram("AIK Archicteture Diagram Solution", show=False):
 
-    
-    rds = RDS("DB-RDS MySQL")
-
-    
-    autoScal = AutoScaling("AutoScaling")
-    loadBal = ELB("ELB")
-    bucket = S3("Bucket 1")
-    bucket2 = S3("Bucket 2")
-    bucket3 = S3("Bucket 3")
+    bucket = S3("Bucket x3")
     vpc = Cluster("VPC")
     
     with vpc:
+        loadBal = ELB("ELB")    
         with Cluster("EC2 Instances - AutoScaling Group"):
-            ec2_1 = EC2("EC2 Web Server")
-            ec2_2 = EC2("EC2 App Server")
-        
-    
+            ec2_1 = EC2("AIK App")
+            ec2_2 = EC2("AIK App")
+            jenkins = EC2("Jenkins CI/CD Server")  
+        with Cluster("DB RDS MySQL"):
+            rds = RDS("")
+   
+
     rds << Edge(label="") >> ec2_1
     rds << Edge(label="") >> ec2_2
-    
+  
     loadBal >> ec2_1
     loadBal >> ec2_2
     
-    ec2_1 >> autoScal
-    ec2_2 >> autoScal
-    
     ec2_1 >> bucket
-    ec2_1 >> bucket2
-    ec2_1 >> bucket3
-    ec2_1 << Cloudwatch("Instance CloudWatch")
+    ec2_2 >> bucket
+    ec2_1 << Cloudwatch("CloudWatch")
 
     
     
