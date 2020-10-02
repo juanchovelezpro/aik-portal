00a806b6c3613aa88f7a7cb5dfd2bf662eb925b9
juanchovelezpro <velezolaya2012@hotmail.com>
2020-10-02 13:28:48 -0500

    Adding terraform template

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "port_front"{
+
+        default = 3030
+        type = string
+        description = "Puerto para el frontend"
+
+    }
+
+variable "port_back"{
+
+        default = 3000
+        type = string
+        description = "Puerto para el backend"
+
+    }
+
+
