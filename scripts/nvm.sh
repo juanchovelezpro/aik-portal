8810e1c00670ca3e61ce81355e16e72509e6a01e
juanchovelezpro <velezolaya2012@hotmail.com>
2020-11-19 23:05:29 -0500

    Cleaning the script

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -5,20 +5,6 @@ sudo yum install -y git
 mkdir -p /srv/app
 sudo chmod 777 /srv/app
 git clone https://github.com/juanchovelezpro/aik-portal /srv/app
-cd /srv/app
-git checkout develop
-git pull
-
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
-#. ~/.nvm/nvm.sh
-#export NVM_DIR="$HOME/.nvm"
-#nvm install node
-#cd /srv/app
-#sudo chmod 777 aik-app-ui
-#cd aik-app-ui
-#sudo chmod 777 package-lock.json
-#sudo chmod 777 package.json
-#npm install
 
 #install salt
 sudo yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm
@@ -26,5 +12,4 @@ sudo yum clean expire-cache;sudo yum -y install salt-minion; chkconfig salt-mini
 #config roles name
 sudo cp -r /srv/app/conf/minion.d /etc/salt/
 echo -e 'grains:\n roles:\n - aik-portal' > /etc/salt/minion.d/grains.conf
-#sudo chown -hR ec2-user /var
 sudo salt-call state.apply > ~/call.log
